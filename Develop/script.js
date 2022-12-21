e6bdb0b7eb677358d0c33b75777d4ad51f39c51d
Eibon <ninetimes64@gmail.com>
2022-12-20 18:30:47 -0600

    added intial variables to the JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
 // Assignment code here
+var upLetter = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d","f", "g", "h", "J", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var lowLetter = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "?"];
 
 
 // Get references to the #generate element
